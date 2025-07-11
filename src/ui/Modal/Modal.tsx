21071da0a5e860b0b4e86c40e322ee19b79d93f6
Dmitry Afonasenko <afonasenko@lanit.ru>
2025-07-11 15:17:47 +0500

    Добавил компонент модалки и портал

--- src/ui/Modal/Modal.tsx
+++ src/ui/Modal/Modal.tsx
@@ -1,16 +1,21 @@
-// import React from 'react';
-// import cls from './Modal.module.scss';
+import React from 'react';
+import cls from './Modal.module.scss';
+import { Button } from '../Button';
 
-// export function Modal({ open, onClose, children }) {
-//     if (!open) return null;
-//     return (
-//         <div className={cls.backdrop} onClick={onClose}>
-//             <div className={cls.modal} onClick={(e) => e.stopPropagation()}>
-//                 <button className={cls.close} onClick={onClose}>
-//                     &times;
-//                 </button>
-//                 {children}
-//             </div>
-//         </div>
-//     );
-// }
+interface ModalProps {
+    open: boolean;
+    onClose: () => void;
+    children: React.ReactNode;
+}
+
+export const Modal: React.FC<ModalProps> = ({ open, onClose, children }) => {
+    if (!open) return null;
+    return (
+        <div className={cls.Modal} onClick={(e) => e.stopPropagation()}>
+            <Button className={cls.ModalClose} onClick={onClose}>
+                &times;
+            </Button>
+            <div className='content'>{children}</div>
+        </div>
+    );
+};
