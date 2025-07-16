a9c62447e582458828b30ae6115dc6ff9b2e7b05
Dmitry Afonasenko <dmitrytbp@gmail.com>
2025-07-16 17:13:04 +0500

    Доработал компонент Select

--- src/ui/Select/Select.tsx
+++ src/ui/Select/Select.tsx
@@ -1,32 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { classNames } from "@/shared/utils/classNames/classNames";
-import cls from "./Select.module.scss"; 
+import cls from "./Select.module.scss";
 
-interface SelectOption {
+export interface SelectOption {
   value: string;
   label: string;
 }
 
 interface SelectProps {
+  label: string;
   options: SelectOption[];
   value: string;
-  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  onChange: (value: string) => void;
   className?: string;
 }
 
 export const Select = (props: SelectProps) => {
-  const { value, onChange, options, className = "" } = props;
+  const { value, onChange, options, className = "", label = "" } = props;
+  const currentIndex = options.findIndex((opt) => opt.value === value);
+  const selectedOption = options[currentIndex] || options[0];
+
+  const handlePrev = () => {
+    const prevIndex = (currentIndex - 1 + options.length) % options.length;
+    onChange(options[prevIndex].value);
+  };
+
+  const handleNext = () => {
+    const nextIndex = (currentIndex + 1) % options.length;
+    onChange(options[nextIndex].value);
+  };
+
   return (
-    <select
-      className={classNames(cls.Select, {}, [className])}
-      value={value}
-      onChange={onChange}
-    >
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
-      ))}
-    </select>
+    <div className={classNames(cls.SelectWrapper, {}, [className])}>
+      <span className={cls.SelectLabel}>{label}</span>
+      <div className={cls.Slider}>
+        <button className={cls.SliderArrow} onClick={handlePrev}>
+          ‹
+        </button>
+
+        <div className={cls.SliderMainWrapp}>
+          <div className={cls.SliderMain}>
+            <div className={cls.SliderValue}>{selectedOption.label}</div>
+          </div>
+
+          <div className={cls.SliderPagination}>
+            {options.map((item, index) => {
+              return (
+                <span
+                  className={classNames(
+                    cls.SliderPaginationItem,
+                    { [cls.ActivePagincation]: currentIndex === index },
+                    []
+                  )}
+                  key={item.value}
+                ></span>
+              );
+            })}
+          </div>
+        </div>
+
+        <button className={cls.SliderArrow} onClick={handleNext}>
+          ›
+        </button>
+      </div>
+    </div>
   );
 };
