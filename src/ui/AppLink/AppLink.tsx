8b910bf00523fa1d28d9c68e747be72694186ec5
Dmitry Afonasenko <dmitrytbp@gmail.com>
2025-07-21 18:00:47 +0500

    Добавил isActive пропс

--- src/ui/AppLink/AppLink.tsx
+++ src/ui/AppLink/AppLink.tsx
@@ -1,35 +1,36 @@
-import { LinkProps, NavLink } from 'react-router-dom';
-import { memo, ReactNode } from 'react';
-import { classNames } from '@/shared/utils/classNames/classNames';
-import cls from './AppLink.module.scss';
-import styles from './AppLink.module.scss';
+import { LinkProps, NavLink } from "react-router-dom";
+import { memo, ReactNode } from "react";
+
+import { classNames } from "@/shared/utils/classNames/classNames";
+
+import cls from "./AppLink.module.scss";
 
 export interface AppLinkProps extends LinkProps {
-    className?: string;
-    children?: ReactNode;
-    activeClassName?: string;
+  className?: string;
+  children?: ReactNode;
+  activeClassName?: string;
+  isActive?: boolean;
 }
 
 export const AppLink = memo((props: AppLinkProps) => {
-    const {
-        to,
-        className = '',
-        children,
-        activeClassName = '',
-        ...otherProps
-    } = props;
+  const {
+    to,
+    className = "",
+    children,
+    activeClassName = "",
+    isActive = false,
+    ...otherProps
+  } = props;
 
-    return (
-        <NavLink
-            to={to}
-            className={({ isActive }) =>
-                classNames(cls.AppLink, { [activeClassName]: isActive }, [
-                    className,
-                ])
-            }
-            {...otherProps}
-        >
-            {children}
-        </NavLink>
-    );
+  return (
+    <NavLink
+      to={to}
+      className={classNames(cls.AppLink, { [activeClassName]: isActive }, [
+        className,
+      ])}
+      {...otherProps}
+    >
+      {children}
+    </NavLink>
+  );
 });
