906d6b2ce4e977b9c7315c0596edf3c49640b344
Dmitry Afonasenko <dmitrytbp@gmail.com>
2025-07-21 18:01:22 +0500

    Добавил classNames

--- src/ui/CyberDivider/CyberDivider.tsx
+++ src/ui/CyberDivider/CyberDivider.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import styles from './CyberDivider.module.scss';
+import React from "react";
+import cls from "./CyberDivider.module.scss";
+import { classNames } from "@/shared/utils/classNames/classNames";
 
 interface CyberDividerProps {
   className?: string;
 }
 
-export const CyberDivider: React.FC<CyberDividerProps> = ({ className = '' }) => (
-  <div className={[styles.CyberDivider, className].filter(Boolean).join(' ')} />
-); 
+export const CyberDivider: React.FC<CyberDividerProps> = ({
+  className = "",
+}) => <div className={classNames(cls.CyberDivider, {}, [className])} />;
