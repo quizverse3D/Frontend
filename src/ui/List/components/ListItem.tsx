08aa8aafbba9c238918cfeb25502ad3366ab57af
Dmitry Afonasenko <dmitrytbp@gmail.com>
2025-09-01 18:52:19 +0500

    Добавил подсказку для текста при длинных значениях

--- src/ui/List/components/ListItem.tsx
+++ src/ui/List/components/ListItem.tsx
@@ -1,22 +1,35 @@
-import React from "react";
 import { classNames } from "@/shared/utils/classNames/classNames";
+
 import cls from "./List.module.scss";
 
 interface ListItemProps {
   className?: string;
-  children: React.ReactNode;
+  title?: string;
+  isHint?: boolean;
   onClickHandler?: () => void;
-  isOnlyText: boolean;
 }
 
+const hideText = (text: string, maxTextLength: number = 50) => {
+  return text.length > maxTextLength
+    ? `${text.slice(0, maxTextLength)}...`
+    : text;
+};
+
 export const ListItem = (props: ListItemProps) => {
-  const { className, children, onClickHandler = undefined } = props;
+  const {
+    className,
+    title = "",
+    isHint = false,
+    onClickHandler = undefined,
+  } = props;
   return (
     <li
       className={classNames(cls.Item, {}, [className])}
       onClick={onClickHandler}
     >
-      {children}
+      <span className={cls.Text} title={title}>
+        {isHint ? hideText(title) : title}
+      </span>
     </li>
   );
 };
